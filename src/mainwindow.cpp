3744b3eedba94eff6d0253c1dff038d240c81326
JackeyLea <1768478912@qq.com>
2020-03-25 15:46:49 +0800

    添加完整代码

---
+++ src/mainwindow.cpp
@@ -0,0 +1,216 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    fileFullPath = QDir::currentPath();
+    ui->lineUrl->setText(fileFullPath);
+    ui->lineResultPath->setText(fileFullPath+QDir::separator()+"test.xml");
+    fileCnt=0;
+    dirCnt=0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::log(QString str)
+{
+    qDebug()<<str;
+    ui->textResult->appendPlainText(str);
+}
+
+void MainWindow::findAllFiles(QString dirPath)
+{
+    if(dirPath.isEmpty()) return;
+    QDir dir(dirPath);
+    QFileInfoList fileInfoList = dir.entryInfoList();
+    foreach(QFileInfo fileInfo , fileInfoList){
+        if(fileInfo.fileName() == "."||fileInfo.fileName()==".."){
+            continue;
+        }
+        else if(fileInfo.isDir()){
+            findAllFiles(fileInfo.filePath());
+            dirCnt++;
+        }
+        else if(fileInfo.isFile()){
+            bool isValid = false;
+            if(fileInfo.suffix()=="html") isValid=true;
+            QString crtFilePath = fileInfo.filePath();
+            QString content = crtFilePath.remove(basePath+QDir::separator());
+            QString title = fileInfo.fileName().split(".").at(0);
+            QString name=title;
+            addFilesNode(isValid,title,name,content);
+            fileCnt++;
+        }
+    }
+}
+
+bool MainWindow::initXML()
+{
+    QDomProcessingInstruction instruction;
+    instruction=doc.createProcessingInstruction("xml","version=\"1.0\" encoding=\"UTF-8\"");
+    doc.appendChild(instruction);
+
+    QDomElement root = doc.createElement("QtHelpProject");
+    root.setAttribute("version","1.0");
+    doc.appendChild(root);
+
+    QDomElement ns=doc.createElement("namespace");
+    QDomText text=doc.createTextNode(ui->lineNamespace->text());
+    ns.appendChild(text);
+    root.appendChild(ns);
+
+    QDomElement vf = doc.createElement("virtualFolder");
+    QDomText vfText = doc.createTextNode("doc");
+    vf.appendChild(vfText);
+    root.appendChild(vf);
+
+    QDomElement fs = doc.createElement("filterSection");
+    QDomElement toc = doc.createElement("toc");
+    //索引内容
+    fs.appendChild(toc);
+    QDomElement kw = doc.createElement("keywords");
+    //关键词
+    fs.appendChild(kw);
+    QDomElement files = doc.createElement("files");
+    //所有文件
+    fs.appendChild(files);
+    root.appendChild(fs);
+
+    log("The basic structure is completed!");
+
+    return true;
+}
+
+bool MainWindow::addFilesNode(bool isValid,QString title,QString name,QString content="test")
+{
+    QDomElement root=doc.documentElement();
+    qDebug()<<root.nodeName();
+    QDomNodeList nodeList = root.childNodes();
+    for(int i=0;i<nodeList.count();i++){
+        QDomNode node = nodeList.at(i);
+        if(node.nodeName()!="filterSection"){
+            continue;
+        }
+        qDebug()<<node.nodeName();
+        QDomNodeList childList = node.childNodes();
+        qDebug()<<childList.count();
+        for(int j=0;j<childList.count();j++){
+            QDomNode child  = childList.at(j);
+            if(child.nodeName()=="toc"){
+                if(!isValid) continue;
+                QDomElement newNode = doc.createElement("section");
+                newNode.setAttribute("title",title);
+                newNode.setAttribute("ref",content);
+                child.appendChild(newNode);
+//            }
+//            else if(child.nodeName()=="keywords"){
+//                if(!isValid) continue;
+//                QDomElement newNode = doc.createElement("keyword");
+//                newNode.setAttribute("name",name);
+//                newNode.setAttribute("ref",content);
+//                child.appendChild(newNode);
+            }else if(child.nodeName()=="files"){
+                QDomNode newNode = doc.createElement("file");
+                QDomText t=doc.createTextNode(content);
+                newNode.appendChild(t);
+                child.appendChild(newNode);
+            }
+        }
+    }
+
+    return true;
+}
+
+bool MainWindow::save2file(QString dstPath)
+{
+    if(dstPath.isEmpty()) throw "There must has dstination file path!";
+    QFile file(dstPath);
+    if(!file.open(QFile::WriteOnly)){
+        file.close();
+        return false;
+    }
+
+    QTextStream writeOut(&file);
+    doc.save(writeOut,4);
+    file.close();
+
+    log("All data has been saved!");
+    return true;
+}
+
+bool MainWindow::generateQch()
+{
+    QString cmd = QString("qhelpgenerator %1 -o %2")
+            .arg(resultFullPath)
+            .arg(dstFullPath);
+    QProcess pro;
+    pro.start(cmd,QProcess::ReadWrite);
+    pro.waitForFinished();
+    log(pro.readAllStandardOutput());
+
+    return true;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    qApp->exit(0);
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QString msg = QString("<h1>QtHelp</h1>"
+                          "<h2>用于快速生成Qt Assistant需要的帮助文档</h2>");
+    QMessageBox::information(this,tr("About"),msg,QMessageBox::Ok);
+}
+
+void MainWindow::on_actionIndex_triggered()
+{
+    QDesktopServices::openUrl(QUrl("https://github.com/JackeyLea/QtHelp"));
+}
+
+void MainWindow::on_actionAbout_Qt_triggered()
+{
+    qApp->aboutQt();
+}
+
+void MainWindow::on_buttonGo_clicked()
+{
+    if(basePath.isEmpty()) basePath=ui->lineUrl->text();
+    ui->textResult->clear();
+    log("Starting initialize xml file content!");
+    initXML();
+
+    findAllFiles(ui->lineUrl->text());
+    QString result=QString("There are %1 files and %2 dirs").arg(fileCnt).arg(dirCnt);
+    log(result);
+
+    if(resultFullPath.isEmpty()) resultFullPath=ui->lineResultPath->text();
+    save2file(resultFullPath);
+
+    generateQch();
+
+    QMessageBox::information(this,tr("Tip"),tr("All operation has been done!\nDo you want to edit it?"),QMessageBox::Ok|QMessageBox::Cancel);
+}
+
+void MainWindow::on_buttonOpen_clicked()
+{
+    ui->lineResultPath->clear();
+    QString resultPath = QFileDialog::getOpenFileName(this,tr("Open"),fileFullPath);
+    if(resultPath.isEmpty()) return;
+    resultFullPath = resultPath;
+    ui->lineResultPath->setText(resultPath);
+}
+
+void MainWindow::on_buttonFolder_clicked()
+{
+    basePath = QFileDialog::getExistingDirectory(this,tr("Open"),fileFullPath);
+    if(basePath.isEmpty()) return;
+    ui->lineUrl->setText(basePath);
+    ui->lineResultPath->setText(basePath);
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,70 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+#include <QDebug>
+#include <QFileDialog>
+#include <QFileInfoList>
+#include <QFileInfo>
+#include <QDir>
+#include <QThread>
+#include <QFile>
+#include <QDesktopServices>
+#include <QMessageBox>
+#include <QtXml>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    QDomDocument doc;
+    QString fileFullPath;
+    QString basePath;
+    QString dstFullPath;
+    QString resultFullPath;
+    int fileCnt;
+    int dirCnt;
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void log(QString str);
+
+    void findAllFiles(QString dirPath);
+
+    bool initXML();
+
+    bool addKeywordsNode(QString content);
+
+    bool addFilesNode(bool isValid, QString title, QString name, QString content);
+
+    bool addTocNode(QString content);
+
+    bool save2file(QString dstPath);
+
+    bool generateQch();
+
+private slots:
+    void on_actionExit_triggered();
+
+    void on_actionAbout_triggered();
+
+    void on_actionIndex_triggered();
+
+    void on_actionAbout_Qt_triggered();
+
+    void on_buttonGo_clicked();
+
+    void on_buttonOpen_clicked();
+
+    void on_buttonFolder_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
